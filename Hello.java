2cf0c75cf0ce164bd2ed3a375fc10cc65d97b865
Jess McDonald <51135336+jessmcdonald@users.noreply.github.com>
2020-04-21 16:06:38 +0200

    initial commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+//class declaration
+public class Hello {
+    // main method groups statements that can be executed
+    public static void main(String[] args) {
+        // System class offered by starndard library
+        System.out.println("Hello World!");
+    }
+}
